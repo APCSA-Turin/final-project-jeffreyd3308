de26762c98e7ad943f40a9b6f23efb9347ba759f
unknown <BT_3N8_17@BKTECH.LOCAL>
2025-05-16 11:34:49 -0400

    anothersave

--- JavaAPIProject/src/main/java/com/example/App.java
+++ JavaAPIProject/src/main/java/com/example/App.java
@@ -3,7 +3,8 @@ package com.example;
 import org.json.JSONObject;
 
 import javax.swing.*;
-import java.util.Scanner;
+
+import java.awt.FlowLayout;
 /**
  * Hello world!
  *
@@ -12,29 +13,18 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-        Scanner scan = new Scanner(System.in);
-        System.out.println( "goober put in city NOW!" );
-        String city = scan.next();
-
-        String endpoint = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=41b37103b67fe0f874e4a4a93ac37cdf";
-        String data = API.getData(endpoint);
-        JSONObject obj = new JSONObject(data);
-        System.out.println(obj);
-        System.out.println(obj.getJSONObject("main").getInt("humidity"));
-
-        //logicals
+        //setup
         JFrame mainframe = new JFrame("mainframe");
-        mainframe.setResizable(false);
-        mainframe.setLayout(null);
-        mainframe.setSize(1920, 1080);
-        mainframe.setVisible(true);
-        Input inputArea = new Input();
-//        inputArea.addActionListener(e -> {
-//            String text = inputArea.getInput().getText();
-//            System.out.println(text);
-//        });
-        mainframe.add(inputArea.getInput());
-
         mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainframe.setResizable(false);
+        mainframe.setLayout(new FlowLayout());
+        mainframe.setSize(400, 300);
+
+        Input inputArea = new Input("Enter a city name.", 20);
+        mainframe.add(inputArea.getText());
+        mainframe.add(inputArea.getInput());
+        mainframe.add(inputArea.getSubmitter());
+        mainframe.setVisible(true);
+
     }
 }
--- JavaAPIProject/src/main/java/com/example/Input.java
+++ JavaAPIProject/src/main/java/com/example/Input.java
@@ -1,24 +1,47 @@
 package com.example;
 //import java.util.Scanner;
 
+import org.json.JSONObject;
+
 import javax.swing.*;
+import java.awt.event.*;
 
 public class Input {
     private JTextField input;
+    private JButton submitter;
+    private JLabel text;
 
     public Input(String defaultString, int size) {
-        input = new JTextField(defaultString, size);
-    }
-
-    public Input(int size) {
+        text = new JLabel(defaultString);
         input = new JTextField(size);
-    }
-
-    public Input() {
-        input = new JTextField();
+        submitter = new JButton("Submit");
+        submitter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String city = input.getText();
+                String endpoint = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=41b37103b67fe0f874e4a4a93ac37cdf";
+                String data = null;
+                try {
+                    data = API.getData(endpoint);
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+                JSONObject obj = new JSONObject(data);
+                System.out.println(obj);
+                System.out.println(obj.getJSONObject("main").getInt("humidity"));
+            }
+        });
     }
 
     public JTextField getInput() {
         return input;
+    }
+
+    public JButton getSubmitter() {
+        return submitter;
+    }
+
+    public JLabel getText() {
+        return text;
     }
 }
